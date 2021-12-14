f81075209c46427997614f1d1b370c623b91f7ce
bloofdoop <75905048+bloofdoop@users.noreply.github.com>
2021-12-14 18:48:57 +0800

    Update tele_bot_testdb.sql

--- tele_bot_testdb.sql
+++ tele_bot_testdb.sql
@@ -1,26 +1,28 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	chat_id varchar(15) not null,
-    username varchar(255) not null,
-    user_id varchar(15) not null,
-    primary key (chat_id),
-    foreign key (user_id) references employees(user_id)
+    username varchar(100) not null,
+    fullname varchar(100) not null,
+    contact_no varchar(25),
+    email varchar(100),
+    primary key (username),
+    foreign key (username) references user_answers(username),
+    foreign key(username) references user_links(username)
 );
 
-DROP TABLE IF EXISTS employees;
-CREATE TABLE employees (
-	user_id varchar(15) not null,
-    fullname varchar(255) not null,
-    primary key (user_id)
-);
-
-DROP TABLE IF EXISTS employee_answers;
-CREATE TABLE employee_answers (
-	user_id varchar(15) not null,
+DROP TABLE IF EXISTS user_answers;
+CREATE TABLE user_answers (
+	username varchar(100) not null,
     question varchar(100) not null,
     answer varchar(255) not null,
-    constraint PK_employee_answers primary key (user_id, question)
+    constraint PK_user_answers primary key (username, question)
 );
 
+DROP TABLE IF EXISTS user_links;
+CREATE TABLE user_links(
+    username varchar(100) not null,
+    link varchar(100) not null,
+    link_description varchar(25) not null,
+    constraint PK_user_links primary key (username, link_description)
+)
 
 
