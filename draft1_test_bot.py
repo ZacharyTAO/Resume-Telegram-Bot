c3bdd51ecd82483577c27950f7e4f8a3e1933820
ikanbilis99 <85048429+ikanbilis99@users.noreply.github.com>
2021-12-27 00:35:37 +0800

    Add files via upload

--- draft1_test_bot.py
+++ draft1_test_bot.py
@@ -20,9 +20,9 @@ logger = logging.getLogger(__name__)
 # Stages
 FIRST, SECOND, THIRD= range(3)
 # Callback data
-ONE, TWO, THREE, FOUR , FIVE, SIX, SEVEN, EIGHT, NINE, TEN, Q1, Q2, Q3, Q4= range(14)
+ONE, TWO, THREE, FOUR , FIVE, SIX, SEVEN, EIGHT, NINE, Q1, Q2, Q3, Q4= range(13)
 
-def edit(update: Update, context: CallbackContext) -> int:
+def main_menu(update: Update, context: CallbackContext) -> int:
     """Send message on `/start`."""
     # Get user that sent /start and log his name
     user = update.message.from_user
@@ -43,6 +43,35 @@ def edit(update: Update, context: CallbackContext) -> int:
     # Tell ConversationHandler that we're in state `FIRST` now
     return FIRST
 
+def back_main_menu(update: Update, context: CallbackContext) -> int:
+    """Prompt same text & keyboard as `start` does but not as new message"""
+    # Get CallbackQuery from Update
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("Particulars", callback_data=str(ONE)),
+            InlineKeyboardButton("Links", callback_data=str(TWO)),
+        ],
+        [
+            InlineKeyboardButton("Q&A", callback_data=str(THREE)),
+            InlineKeyboardButton("Confirmation Menu", callback_data=str(FOUR)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    query.edit_message_text(text="Main Menu", reply_markup=reply_markup)
+    return FIRST
+
+def done(update: Update, context: CallbackContext) -> int:
+    """Returns `ConversationHandler.END`, which tells the
+    ConversationHandler that the conversation is over.
+    """
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="See you next time!")
+    return ConversationHandler.END
+
 def particulars(update: Update, context: CallbackContext) -> int:
     """Show new choice of buttons"""
     query = update.callback_query
@@ -57,7 +86,6 @@ def particulars(update: Update, context: CallbackContext) -> int:
         text="Once you are done editing, click on 'confirmation menu'", reply_markup=reply_markup
     )
     return FIRST
-
 
 def links(update: Update, context: CallbackContext) -> int:
     """Show new choice of buttons"""
@@ -91,17 +119,17 @@ def qna(update: Update, context: CallbackContext) -> int:
         text=
         "1. Describe yourself\n\n"+ 
 
+        "Question 1 answer\n\n"+
+
         "2. Why are you applying for this job?\n\n"+ 
+
+        "Question 2 answer\n\n"+
 
         "3. What are some of your strengths and weaknesses?\n\n"+
 
-        "4. What are some challenges you have experienced and how did you overcome it?\n\n"+ 
-        
-        "Question 1 answer\n\n"+
-
-        "Question 2 answer\n\n"+
-        
         "Question 3 answer\n\n"+
+
+        "4. What are some challenges you have experienced and how did you overcome it?\n\n"+ 
 
         "Question 4 answer\n\n"
 
@@ -122,11 +150,69 @@ def question_menu(update: Update, context: CallbackContext) -> int:
         [
             InlineKeyboardButton("Q3", callback_data=str(Q3)),
             InlineKeyboardButton("Q4", callback_data=str(Q4)),
-        ]
+        ],
+        [
+            InlineKeyboardButton("Back", callback_data=str(THREE))
+        ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(
-        text="select a question", reply_markup=reply_markup
+        text=
+        "1. Describe yourself\n\n"+ 
+
+        "Question 1 answer\n\n"+
+
+        "2. Why are you applying for this job?\n\n"+ 
+
+        "Question 2 answer\n\n"+
+
+        "3. What are some of your strengths and weaknesses?\n\n"+
+
+        "Question 3 answer\n\n"+
+
+        "4. What are some challenges you have experienced and how did you overcome it?\n\n"+ 
+
+        "Question 4 answer\n\n"
+        , reply_markup=reply_markup
+    )
+    return THIRD
+
+def back_question_menu(update: Update, context: CallbackContext) -> int:
+    """Show new choice of buttons"""
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton("Q1", callback_data=str(Q1)),
+            InlineKeyboardButton("Q2", callback_data=str(Q2)),
+        ],
+        [
+            InlineKeyboardButton("Q3", callback_data=str(Q3)),
+            InlineKeyboardButton("Q4", callback_data=str(Q4)),
+        ],
+        [
+            InlineKeyboardButton("Back", callback_data=str(THREE))
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text=
+        "1. Describe yourself\n\n"+ 
+
+        "Question 1 answer\n\n"+
+
+        "2. Why are you applying for this job?\n\n"+ 
+
+        "Question 2 answer\n\n"+
+
+        "3. What are some of your strengths and weaknesses?\n\n"+
+
+        "Question 3 answer\n\n"+
+
+        "4. What are some challenges you have experienced and how did you overcome it?\n\n"+ 
+
+        "Question 4 answer\n\n"
+        , reply_markup=reply_markup
     )
     return THIRD
 
@@ -135,12 +221,12 @@ def question1(update: Update, context: CallbackContext) -> int:
     query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Confirmation Menu", callback_data=str(FOUR)),
+            InlineKeyboardButton("Back", callback_data=str(EIGHT)),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(
-        text="Input answer for question 1. \n\n Press enter to confirm answer. \n\n Click on 'confirmation menu' to go back or end.\n\n", reply_markup=reply_markup
+        text="Input answer for question 1. \n\n Press enter to confirm answer. \n\n Click on 'back' to return to question menu.\n\n", reply_markup=reply_markup
     )
     return FIRST
 
@@ -149,26 +235,42 @@ def question2(update: Update, context: CallbackContext) -> int:
     query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Confirmation Menu", callback_data=str(FOUR)),
+            InlineKeyboardButton("Back", callback_data=str(EIGHT)),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(
-        text="Input answer for question 1. \n\n Press enter to confirm answer. \n\n Click on 'confirmation menu' to go back or end.\n\n",reply_markup=reply_markup
+        text="Input answer for question 2. \n\n Press enter to confirm answer. \n\n Click on 'back' to return to question menu.\n\n",reply_markup=reply_markup
     )
     return FIRST
 
 def question3(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text="q3")
-    return THIRD
+    keyboard = [
+        [
+            InlineKeyboardButton("Back", callback_data=str(EIGHT)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Input answer for question 3. \n\n Press enter to confirm answer. \n\n Click on 'back' to return to question menu.\n\n",reply_markup=reply_markup
+    )
+    return FIRST
 
 def question4(update: Update, context: CallbackContext) -> int:
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text="q4")
-    return THIRD
+    keyboard = [
+        [
+            InlineKeyboardButton("Back", callback_data=str(EIGHT)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Input answer for question 4. \n\n Press enter to confirm answer. \n\n Click on 'back' to return to question menu.\n\n",reply_markup=reply_markup
+    )
+    return FIRST
 
 def confirmation_menu(update: Update, context: CallbackContext) -> int:
     """Show new choice of buttons"""
@@ -186,36 +288,6 @@ def confirmation_menu(update: Update, context: CallbackContext) -> int:
     )
     return SECOND
 
-def back(update: Update, context: CallbackContext) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
-    query = update.callback_query
-    query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("Particulars", callback_data=str(ONE)),
-            InlineKeyboardButton("Links", callback_data=str(TWO)),
-        ],
-        [
-            InlineKeyboardButton("Q&A", callback_data=str(THREE)),
-            InlineKeyboardButton("Confirmation Menu", callback_data=str(FOUR)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    query.edit_message_text(text="Main Menu", reply_markup=reply_markup)
-    return FIRST
-
-def done(update: Update, context: CallbackContext) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text="See you next time!")
-    return ConversationHandler.END
-
-
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
@@ -225,27 +297,30 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', edit)],
+        entry_points=[CommandHandler('start', main_menu)],
         states={
             FIRST: [
                 CallbackQueryHandler(particulars, pattern='^' + str(ONE) + '$'),
                 CallbackQueryHandler(links, pattern='^' + str(TWO) + '$'),
                 CallbackQueryHandler(qna, pattern='^' + str(THREE) + '$'),
                 CallbackQueryHandler(question_menu, pattern='^' + str(SEVEN) + '$'),
+                CallbackQueryHandler(back_question_menu, pattern='^' + str(EIGHT) + '$'),
                 CallbackQueryHandler(confirmation_menu, pattern='^' + str(FOUR) + '$'),
+                
             ],
             SECOND: [
-                CallbackQueryHandler(back, pattern='^' + str(FIVE) + '$'),
+                CallbackQueryHandler(back_main_menu, pattern='^' + str(FIVE) + '$'),
                 CallbackQueryHandler(done, pattern='^' + str(SIX) + '$'),  
             ],
             THIRD: [
+                CallbackQueryHandler(qna, pattern='^' + str(THREE) + '$'),
                 CallbackQueryHandler(question1, pattern='^' + str(Q1) + '$'),
                 CallbackQueryHandler(question2, pattern='^' + str(Q2) + '$'),
                 CallbackQueryHandler(question2, pattern='^' + str(Q3) + '$'), 
                 CallbackQueryHandler(question2, pattern='^' + str(Q4) + '$'),  
             ]
         },
-        fallbacks=[CommandHandler('start', edit)],
+        fallbacks=[CommandHandler('start', main_menu)],
     )
 
     # Add ConversationHandler to dispatcher that will be used for handling updates
